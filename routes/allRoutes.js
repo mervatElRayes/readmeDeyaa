c87580fadaafc72d8a44a2eadd16e16cc5bc8067
mervatElRayes <mervatel99@gmail.com>
2026-03-06 15:20:35 +0200

    verificationL6

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const AuthUser = require("../models/authUser")
+const bcrypt = require('bcrypt');
+
+
 // Level 2
 router.get("/",  (req, res) => {
 res.render("welcome")
@@ -30,6 +33,31 @@ res.redirect("/")
 } catch (error) {
     console.log(error);
 }
+
+});
+
+// check if email correct Or No
+router.post("/login", async (req, res) => {
+ console.log("7777777777777777777777777");
+const logInUser = await AuthUser.findOne({email: req.body.email})
+ console.log(logInUser);
+
+if (logInUser === null) {
+    console.log("This email not Found In DB");
+    
+}else{
+    const match = await bcrypt.compare(req.body.password, logInUser.password)
+    if (match) {
+        console.log("correct Email & Password");
+    }else{
+        console.log("wrong password");
+    }
+
+    res.redirect("/signup")
+    
+}
+
+
 
 });
 
